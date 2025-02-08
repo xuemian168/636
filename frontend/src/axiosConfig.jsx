845ce26cbc789d12b68205529275c0e58ff2a473
Md Mahfujur Rahman <mdmahfujurrahman@Mds-MacBook-Pro.local>
2025-02-09 01:07:22 +1000

    server modified

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.239.118.224:5001', // live
+  baseURL: 'http://3.25.252.187:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
